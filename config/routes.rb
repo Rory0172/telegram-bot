d5e439a08793213bc71374dfd25535931e4a64ba
Rory0172 <rory0172@live.nl>
2018-08-08 09:36:29 +0200

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  post '/webhooks/telegram_vbc43edbf1kljlkekjsfhskfguhesfkshu614a075954dvd4bfab34l1' => 'webhooks#callback'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
