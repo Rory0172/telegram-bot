d5f646c388a85d9a384dc29d0d5a2a36577d730f
Rory0172 <rory0172@live.nl>
2018-08-17 15:51:06 +0200

    add time stamp

--- app/lib/coin_bot.rb
+++ app/lib/coin_bot.rb
@@ -8,7 +8,7 @@ class CoinBot < Bot
       if @signal.blank?
         reply ({chat_id: msg.chat.id, text:"No signals given yet!"})
       else
-        reply({chat_id: msg.chat.id, text:"*Target #{@coin.name} (#{@signal.exchange})*\nTimestamp:{@signal.created_at}\nCurrent price: #{@coin.current_price}\nResult: #{@signal.result}\nEntry: #{@signal.entry_price}\nTarget 1: #{@signal.sell_target_1}\nTarget 2:#{@signal.sell_target_2}\nStoploss: #{@signal.stoploss}", parse_mode:"markdown"})
+        reply({chat_id: msg.chat.id, text:"*Target #{@coin.name} (#{@signal.exchange})*\n#{@signal.time_ago}\nCurrent price: #{@coin.current_price}\nResult: #{@signal.result}\nEntry: #{@signal.entry_price}\nTarget 1: #{@signal.sell_target_1}\nTarget 2: #{@signal.sell_target_2}\nStoploss: #{@signal.stoploss}", parse_mode:"markdown"})
       end
     end
   end
@@ -17,7 +17,7 @@ class CoinBot < Bot
     @coin = Coin.find(signal.coin.id)
     Telegram::Bot::Client.run(@token) do |bot|
       User.all.each do |user|
-        bot.api.send_message(chat_id: user.chat_id, text:"*New Target #{@coin.name} (#{signal.exchange})*\nCurrent price: #{@coin.current_price}\nResult: #{signal.result}\nEntry: #{signal.entry_price}\nTarget 1: #{signal.sell_target_1}\nTarget 2:#{signal.sell_target_2}\nStoploss: #{signal.stoploss}", parse_mode:"markdown")
+        bot.api.send_message(chat_id: user.chat_id, text:"*New Target #{@coin.name} (#{signal.exchange})*\nCurrent price: #{@coin.current_price}\nResult: #{signal.result}\nEntry: #{signal.entry_price}\nTarget 1: #{signal.sell_target_1}\nTarget 2: #{signal.sell_target_2}\nStoploss: #{signal.stoploss}", parse_mode:"markdown")
       end
     end
   end
--- app/models/coin_signal.rb
+++ app/models/coin_signal.rb
@@ -9,4 +9,20 @@ class CoinSignal < ApplicationRecord
   def announce
     CoinBot.new(ENV["BOTTOKEN"]).announce(self)
   end
+
+  def time_ago
+    diff_seconds = Time.now - created_at
+    case diff_seconds
+      when 0 .. 59
+        "#{diff_seconds} seconds ago"
+      when 60 .. (3600-1)
+        "#{(diff_seconds/60).round} minutes ago"
+      when 3600 .. (3600*24-1)
+        "#{(diff_seconds/3600).round} hours ago"
+      when (3600*24) .. (3600*24*30) 
+        "#{(diff_seconds/(3600*24)).round} days ago"
+      else
+        start_time.strftime("%m/%d/%Y")
+    end
+  end
 end
