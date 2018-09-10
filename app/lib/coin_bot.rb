48481e609cc28778c6b204de76fd21bfa2f13aa3
Rory0172 <rory0172@live.nl>
2018-09-10 13:18:45 +0200

    add function to set current price

--- app/lib/coin_bot.rb
+++ app/lib/coin_bot.rb
@@ -1,13 +1,27 @@
 class CoinBot < Bot
+
+  def set_price(coin_name)
+    @coin = Coin.find_by(name: coin_name)
+    btc = Binance::Api.ticker!(symbol: "BTCUSDT", type: "price")
+    if coin_name == "BTC"
+      @coin.current_price = btc[:price]
+      @coin.save
+    else
+      binance_data_coin = Binance::Api.ticker!(symbol: "#{coin_name}BTC", type: "price")
+      price_btc = btc[:price].to_f
+      @coin.current_price = binance_data_coin[:price].to_f * price_btc
+      @coin.save
+    end
+  end
+
   def message(msg)
     if msg.text.match /TARGETS/i
       if CoinSignal.all.blank?
         reply({chat_id: msg.chat.id, text:"There are no signals given yet."})
       else
-        reply({chat_id: msg.chat.id, text:"Click on a signal to get detailed information or use /coinname.", parse_mode:"markdown"})
-        CoinSignal.all.each do |signal|
-          reply({chat_id: msg.chat.id, text:"/#{signal.coin.name} (#{signal.exchange}): #{signal.result} #{signal.result.to_f < 0 ? "\u{2B07}" : "\u{2B06}"}", parse_mode:"markdown"})
-        end
+        text = "Click on a signal to get detailed information or use */coinname*.\n\nActive signals:\n"
+        CoinSignal.all.each{|signal| text << "/#{signal.coin.name} (#{signal.exchange}): #{signal.result} #{signal.result.to_f < 0 ? "\u{2B07}" : "\u{2B06}"}\n"}
+        reply({chat_id: msg.chat.id, text: text, parse_mode:"markdown"})
       end
     end
 
@@ -16,15 +30,7 @@ class CoinBot < Bot
       if @coin.blank?
         reply ({chat_id: msg.chat.id, text:"There is no signal given for #{msg.text}. Please use /targets to get an overview for active targets."})
       else
-        btc = Binance::Api.ticker!(symbol: "BTCUSDT", type: "price")
-        if msg.text == "BTC"
-          @coin.current_price = btc[:price]
-        else
-          current_states_coin = Binance::Api.ticker!(symbol: "#{msg.text}BTC", type: "price")
-          price_btc = btc[:price].to_f
-          @coin.current_price = current_states_coin[:price].to_f * price_btc
-          @coin.save
-        end
+        self.set_price(msg.text)
         @signal = @coin.coin_signals.last
         if @signal.blank?
           reply ({chat_id: msg.chat.id, text:"No signals given yet!"})
@@ -36,10 +42,10 @@ class CoinBot < Bot
   end
 
   def announce(signal)
-    @coin = Coin.find(signal.coin.id)
+    self.set_price(signal.coin.name)
     Telegram::Bot::Client.run(@token) do |bot|
       User.all.each do |user|
-        bot.api.send_message(chat_id: user.chat_id, text:"*New Target #{@coin.name} (#{signal.exchange})*\nCurrent price: #{@coin.current_price}\nResult: #{signal.result}\nEntry: #{signal.entry_price}\nTarget 1: #{signal.sell_target_1}\nTarget 2: #{signal.sell_target_2}\nStoploss: #{signal.stoploss}", parse_mode:"markdown")
+        bot.api.send_message(chat_id: user.chat_id, text:"\u{26A1} *NEW TARGET* \u{26A1}\n\n*#{@coin.name} (#{signal.exchange})*\nEntry: #{signal.entry_price}\nCurrent price: #{@coin.current_price}\n\nTarget 1: #{signal.sell_target_1}\nTarget 2: #{signal.sell_target_2}\nStoploss: #{signal.stoploss}", parse_mode:"markdown")
       end
     end
   end
