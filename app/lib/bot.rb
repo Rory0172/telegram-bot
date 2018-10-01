d61a6beaffcbf9a2fd62cdd08fda17aae31615cf
Rory0172 <rory0172@live.nl>
2018-10-01 11:04:36 +0200

    add 1 time login

--- app/lib/bot.rb
+++ app/lib/bot.rb
@@ -17,9 +17,14 @@ class Bot
           when Telegram::Bot::Types::CallbackQuery
             self.callback(message)
           when Telegram::Bot::Types::Message
-            @user = User.find_or_create_by(telegram_id: message.from.id, username: message.from.username, chat_id:message.from.id)
-            text = message.text
-            self.message(message, text)
+            @user = User.find_by(telegram_id: message.from.id)
+            if @user
+              text = message.text
+              self.message(message, text)
+            else
+              User.create(telegram_id: message.from.id, username: message.from.username, chat_id:message.from.id)
+              reply ({chat_id: message.from.id, text:"Welcome to the *DCT* bot!\nThis bot will help you organize crypto signals and alert you when targets are hit. This bot can listen to your commands:\n\n*Help*\nNot sure how to get started? Just type /help\n\n*Signals*\nGet an overview of our current signals using /signals\n\n*Coinname*\nGet the status on an active signal by using the right abbreviation of a coin. _For example: /btc_", parse_mode:"markdown"})
+            end
           end
         else
           reply ({chat_id: message.from.id, text:"You are not a member of the premium group *#{chat_title}*. If you would like to access this bot, please contact @admin.", parse_mode:"markdown"})
