b1b3fecfc8a09baf44f75a1bda3c4498e1a3b1a1
Rory0172 <rory0172@live.nl>
2018-10-03 19:16:35 +0200

    fix coin abbreviation

--- app/lib/bot.rb
+++ app/lib/bot.rb
@@ -1,33 +1,34 @@
 require 'telegram/bot'
 class Bot
   def initialize(token)
-    @token=token
+    @token = token
   end
+
   def start_listening
     Telegram::Bot::Client.run(@token) do |bot|
       @bot = bot
       @bot.listen do |message|
         Rails.logger.info message
-        chat_member = @bot.api.get_chat_member(chat_id:ENV["CHATID"], user_id: message.from.id)
-        chat = @bot.api.get_chat(chat_id:ENV["CHATID"])
+        chat_member = @bot.api.get_chat_member(chat_id: ENV['CHATID'], user_id: message.from.id)
+        chat = @bot.api.get_chat(chat_id: ENV['CHATID'])
         puts chat_member
-        chat_title = chat["result"]["title"]
-        if chat_member["result"]["status"] != "left" and chat_member["result"]["status"] != "kicked"
+        chat_title = chat['result']['title']
+        if (chat_member['result']['status'] != 'left') && (chat_member['result']['status'] != 'kicked')
           case message
           when Telegram::Bot::Types::CallbackQuery
-            self.callback(message)
+            callback(message)
           when Telegram::Bot::Types::Message
             @user = User.find_by(telegram_id: message.from.id)
             if @user
               text = message.text
               self.message(message, text)
             else
-              User.create(telegram_id: message.from.id, username: message.from.username, chat_id:message.from.id)
-              reply ({chat_id: message.from.id, text:"Welcome to the *DCT* bot!\nThis bot will help you organize crypto signals and alert you when targets are hit. This bot can listen to your commands:\n\n*Help*\nNot sure how to get started? Just type /help\n\n*Signals*\nGet an overview of our current signals using /signals\n\n*Coinname*\nGet the status on an active signal by using the right abbreviation of a coin. _For example: /btc_", parse_mode:"markdown"})
+              User.create(telegram_id: message.from.id, username: message.from.username, chat_id: message.from.id)
+              reply ({ chat_id: message.from.id, text: "Welcome to the *DCT* bot!\nThis bot will help you organize crypto signals and alert you when targets are hit. This bot can listen to your commands:\n\n*Help*\nNot sure how to get started? Just type /help\n\n*Signals*\nGet an overview of our current signals using /signals\n\n*Coinname*\nGet the status on an active signal by using the right abbreviation of a coin. _For example: /btc_", parse_mode: 'markdown' })
             end
           end
         else
-          reply ({chat_id: message.from.id, text:"You are not a member of the premium group *#{chat_title}*. If you would like to access this bot, please contact @admin.", parse_mode:"markdown"})
+          reply ({ chat_id: message.from.id, text: "You are not a member of the premium group *#{chat_title}*. If you would like to access this bot, please contact @admin.", parse_mode: 'markdown' })
         end
       end
     end
@@ -37,21 +38,15 @@ class Bot
     @user
   end
 
-  def callback(msg)
-  end
+  def callback(msg); end
 
-  def message(msg)
-  end
+  def message(msg); end
 
-  def announce(signal)
-  end
+  def announce(signal); end
 
   def reply(msg)
-    begin
-      @bot.api.send_message msg
-    rescue Exception => e
-      Rails.logger.info e
-    end
+    @bot.api.send_message msg
+  rescue Exception => e
+    Rails.logger.info e
   end
 end
-
--- app/lib/coin_bot.rb
+++ app/lib/coin_bot.rb
@@ -29,7 +29,8 @@ class CoinBot < Bot
 
   def message(msg, text)
     if text != nil
-      if text.match /signals/i
+      case text
+      when /signals/
         if CoinSignal.all.blank?
           reply({chat_id: msg.from.id, text:"There are no signals given yet."})
         else
@@ -37,30 +38,26 @@ class CoinBot < Bot
           CoinSignal.all.each{|signal| message << "/#{signal.coin.name} (#{signal.exchange}): #{signal.result} #{signal.result.to_f < 0 ? "\u{2B07}" : "\u{2B06}"}\n"}
           reply({chat_id: msg.from.id, text: message, parse_mode:"markdown"})
         end
-      end
-
-      if text.match /\/\b[A-Za-z]{3}\b$/i
-        @coin = Coin.find_by(name: text[1..3].upcase)
-        if @coin.blank?
-          reply ({chat_id: msg.from.id, text:"Can’t find this specific coin. Make sure that you use the correct abbreviation or check /signals for active signals.  "})
-        else
-          self.set_price(text[1..3].upcase)
-          @signal = @coin.coin_signals.last
-          if @signal.blank?
-            reply ({chat_id: msg.from.id, text:"There is no signal given for *#{text[1..3].upcase}*. Please use /signals to get an overview for active signals.", parse_mode:"markdown"})
-          else
-            reply({chat_id: msg.from.id, text:"*Target #{@coin.name} (#{@signal.exchange})*\n#{@signal.time_ago} ago\nCurrent price: #{@coin.current_price}\nResult: #{@signal.result} #{@signal.result.to_f < 0 ? "\u{2B07}" : "\u{2B06}"}\nEntry: #{@signal.entry_price_low} - #{@signal.entry_price_high}\nTarget 1: #{@signal.sell_target_1_low} - #{@signal.sell_target_1_high}\nTarget 2: #{@signal.sell_target_2_low} - #{@signal.sell_target_2_high}\nStoploss: #{@signal.stoploss}#{"\nNote: #{@signal.note}" unless @signal.note.blank?}", parse_mode:"markdown"})
-          end
-        end
-      end
-
-      if text.match /help/i
+      when /help/
         kb = [
           Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Get signal for a coin', callback_data: 'signal'),
           Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Get signal overview', callback_data: 'signals')
         ]
         markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
         reply({chat_id: msg.chat.id, text: 'This bot will help you organize crypto signals and alert you when targets are hit. How can I help you?', reply_markup: markup})
+      else
+        @coin = Coin.find_by(name: text.upcase)
+        if @coin.blank?
+          reply ({chat_id: msg.from.id, text:"Can’t find this specific coin. Make sure that you use the correct abbreviation or check /signals for active signals.  "})
+        else
+          self.set_price(text.upcase)
+          @signal = @coin.coin_signals.last
+          if @signal.blank?
+            reply ({chat_id: msg.from.id, text:"There is no signal given for *#{text.upcase}*. Please use /signals to get an overview for active signals.", parse_mode:"markdown"})
+          else
+            reply({chat_id: msg.from.id, text:"*Target #{@coin.name} (#{@signal.exchange})*\n#{@signal.time_ago} ago\nCurrent price: #{@coin.current_price}\nResult: #{@signal.result} #{@signal.result.to_f < 0 ? "\u{2B07}" : "\u{2B06}"}\nEntry: #{@signal.entry_price_low} - #{@signal.entry_price_high}\nTarget 1: #{@signal.sell_target_1_low} - #{@signal.sell_target_1_high}\nTarget 2: #{@signal.sell_target_2_low} - #{@signal.sell_target_2_high}\nStoploss: #{@signal.stoploss}#{"\nNote: #{@signal.note}" unless @signal.note.blank?}", parse_mode:"markdown"})
+          end
+        end
       end
     end
   end
